1344f183a8f4991b5928e3186ea7eee67d799486
iamdrewkirkland <drew.kirkland@gmail.com>
2020-04-17 17:10:56 -0400

    All prompts respond succesfully and alerts display current list of pottery and their ID.

--- scripts/pottery/PotteryList.js
+++ scripts/pottery/PotteryList.js
@@ -3,18 +3,10 @@ let potteryId = 1;
 let selectedPottery = 0;
 let temp = 0;
 
-// const makePottery = () => {
-//   //prompt "Are you ready?"
-//   inputDimensions();
-//   //prompt "Now let's fire it!"
-//   selectPottery(pottery);
-//   firePottery(selectedPottery, temp);
-// };
-
 export const inputDimensions = () => {
-  const shape = prompt("What shape is the pottery?");
-  const weight = prompt("How heavy is the pottery?");
-  const height = prompt("How tall is the pottery?");
+  const shape = prompt("What shape is the pottery (plate, cup, saucer, bowl)?");
+  const weight = prompt("How heavy (pounds) is the pottery?");
+  const height = prompt("How tall (inches) is the pottery?");
 
   shapePottery(shape, weight, height);
 };
@@ -29,27 +21,37 @@ const shapePottery = (shape, weight, height) => {
 
   pottery.push(newPotteryObject);
   potteryId++;
-  kilnSetup(pottery)
+  kilnSetup(pottery);
 };
 
 const kilnSetup = (potteryArray) => {
-  potteryArray.map((pottery) => console.log(pottery.id && pottery.shape));
+  alert(
+    potteryArray.map(
+      (pottery) =>
+        `
+        ID:${pottery.id} 
+        SHAPE:${pottery.shape} 
+        WEIGHT:${pottery.weight} 
+        HEIGHT: ${pottery.height}
+        `
+    )
+  );
   let potteryNumber = parseInt(
     prompt("Enter the number of the pottery piece you would like", "1")
   );
-  selectedPottery = potteryArray[potteryNumber + 1];
+  selectedPottery = potteryArray[potteryNumber - 1];
   temp = parseInt(prompt("Enter a temperature (intervals of 100)", "100"));
-  firePottery(selectedPottery, temp)
+  firePottery(selectedPottery, temp);
 };
 
 const firePottery = (potteryObject, temp) => {
   if (temp > 2200) {
-    potteryObject.cracked = true
+    potteryObject.cracked = true;
+    return console.log(pottery);
   } else {
-      potteryObject.cracked = false
+    potteryObject.cracked = false;
+    return console.log(pottery);
   }
 };
 
-const evaluatePottery = (potteryArray) => {
-
-}
+const evaluatePottery = (potteryArray) => {};
