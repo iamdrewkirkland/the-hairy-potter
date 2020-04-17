6cbab5fbdeb89a49f5fda8b39aab1227153a9e7e
iamdrewkirkland <drew.kirkland@gmail.com>
2020-04-17 15:04:01 -0400

    Further implementing logic for makePottery, inputDimensions, and firePottery functions.

--- scripts/pottery/PotteryList.js
+++ scripts/pottery/PotteryList.js
@@ -1,15 +1,28 @@
 let pottery = []
 let potteryId = 1
 
+
+const inputDimensions = () => {
+    const shape = prompt('What shape is the pottery?')
+    const weight = prompt('How heavy is the pottery?')
+    const height = prompt('How tall is the pottery?')
+
+    makePottery(shape, weight, height)
+}
+
 const makePottery = (shape, weight, height) => {
     const newPotteryObject = {
-        id: potteryId
+        id: potteryId,
         shape: shape,
         weight: weight,
-        height: height,
+        height: height
     }
     
     pottery.push(newPotteryObject)
     potteryId++
 
+}
+
+const firePottery = (potteryObject, temperature) => {
+    
 }
