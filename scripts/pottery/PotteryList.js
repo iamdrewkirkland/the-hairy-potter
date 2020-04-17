0eb68fc28fac8243abe968d6a8cdaa1023f031ad
iamdrewkirkland <drew.kirkland@gmail.com>
2020-04-17 15:34:24 -0400

    Further logic refinement.

--- scripts/pottery/PotteryList.js
+++ scripts/pottery/PotteryList.js
@@ -1,28 +1,47 @@
-let pottery = []
-let potteryId = 1
+let pottery = [];
+let potteryId = 1;
+let selectedPottery = 0;
+let temp = 0;
 
+// const makePottery = () => {
+//   //prompt "Are you ready?"
+//   inputDimensions();
+//   //prompt "Now let's fire it!"
+//   selectPottery(pottery);
+//   firePottery(selectedPottery);
+// };
 
 const inputDimensions = () => {
-    const shape = prompt('What shape is the pottery?')
-    const weight = prompt('How heavy is the pottery?')
-    const height = prompt('How tall is the pottery?')
+  const shape = prompt("What shape is the pottery?");
+  const weight = prompt("How heavy is the pottery?");
+  const height = prompt("How tall is the pottery?");
 
-    makePottery(shape, weight, height)
-}
+  shapePottery(shape, weight, height);
+};
 
-const makePottery = (shape, weight, height) => {
-    const newPotteryObject = {
-        id: potteryId,
-        shape: shape,
-        weight: weight,
-        height: height
-    }
-    
-    pottery.push(newPotteryObject)
-    potteryId++
+const shapePottery = (shape, weight, height) => {
+  const newPotteryObject = {
+    id: potteryId,
+    shape: shape,
+    weight: weight,
+    height: height,
+  };
 
-}
+  pottery.push(newPotteryObject);
+  potteryId++;
+};
+
+const kilnSetup = (potteryArray) => {
+  potteryArray.map((pottery) => console.log(pottery.id && pottery.shape));
+  let potteryNumber = parseInt(
+    prompt("Enter the number of the pottery piece you would like:", "1")
+  );
+  selectedPottery = potteryArray[potteryNumber + 1];
+  temp = parseInt(prompt("Enter a temperature (intervals of 100)", "100"));
+};
 
 const firePottery = (potteryObject, temperature) => {
-    
-}
+  if (temp > 2200) {
+      
+  }
+};
